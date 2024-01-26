e7cc0f5b62beea5eeb503a715412a22c676eaad5
fbartsch <felixbartsch@gmail.com>
2024-01-26 16:02:41 -0500

    Fixed dDPI gain calculation, added get_sta function, fixed ExtractAndAlign for LSR use, and other fixes

---
+++ Step0_KilosortLaminar.m
@@ -0,0 +1,102 @@
+function [datPath, droptestcheck] = Step0_KilosortLaminar(dirpath,filenameP,preconverted)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+if nargin <3
+    preconverted=0;
+end
+%%
+cd(dirpath)
+if dirpath(end) ~= filesep
+	dirpath = [dirpath filesep];
+end
+outputFolder = [dirpath filenameP '/kilosorting_laminar/'];
+
+disp('Starting conversion')
+
+opts.commonAverageReferencing = false; % we do not want to do digital rereferencing here, better to do in Plexon
+opts.removeArtifacts = false; % enter at own risk!
+opts.plotProbeVoltage = false;
+opts.extractLfp = false;
+opts.outputFolder = outputFolder;
+
+opts.nChans = 24; % laminar probes will usually have 24 channels, but this can be set to 32 depending on the probe
+opts.ChnOffset=0;
+opts.batch_size = 5e9; % batch size prevents memory overflow errors. default = 300000000 for IT
+%opts.specificChannels = [];  % user can select which plexon channels to use for
+%                               conversion. remember, this must be in plexon-numbering.
+%%
+try
+    [fs, n, ts, fn, ~] = plx_ad_v([filenameP '.pl2'], ['SPKC001'] );
+catch
+    [fs, n, ts, fn, ~] = plx_ad_v([filenameP '.pl2'], ['SPKC01'] );
+end
+[~, n_aux, ts_aux, fn_aux, ~] = plx_ad_v([filenameP '.pl2'], ['AI01'] );
+if n<2; error('error reading laminar probe'); end
+%%
+droptestcheck = [n/40000- n_aux/1000];
+%%
+if ~preconverted
+    %convert pl2 file into dat format
+    [samples, datPath] = convertRawToDat_Laminar([filenameP '.pl2'],opts);
+else
+    datPath = [dirpath filenameP '/kilosorting_laminar/' filenameP '.dat'];
+end
+%datPath = ['C:\SpkSort2023\AAActiveData\' filenameP
+%'\kilosorting_laminar\' filenameP '.dat'];%uncommented for spike sorting a session for subsequent times
+
+disp('Done with conversion')
+%% Probe geometry
+%[xcoords, ycoords, kcoords] = probeGeometry2coords('linear50', 24); % now
+%lives in mastermegafile
+
+%% edit the Megafile, save it into the target directory
+%
+
+fs = 40000; %default
+nCh = opts.nChans; % = size(samples,1);
+
+% set Parameters: 
+% frequency for high pass filtering (150)
+%ops.fshigh = 150; % KS2.5  
+ops.fshigh = 300; % KS3: high-pass more aggresively
+
+% minimum firing rate on a "good" channel (0 to skip)
+ops.minfr_goodchannels = 0; 
+
+% threshold on projections (like in Kilosort1, can be different for last pass like [10 4])
+ops.Th = [9 5]; % KS2.5
+%ops.Th = [9 9];  % KS3
+
+% how important is the amplitude penalty (like in Kilosort1, 0 means not used, 10 is average, 50 is a lot) 
+ops.lam = 10;  
+
+% splitting a cluster at the end requires at least this much isolation for each sub-cluster (max = 1)
+ops.AUCsplit = 0.9; 
+
+% minimum spike rate (Hz), if a cluster falls below this for too long it gets removed
+ops.minFR = 0; 
+
+% number of samples to average over (annealed from first to second value) 
+ops.momentum = [20 400]; 
+
+% spatial constant in um for computing residual variance of spike
+ops.sigmaMask  = 30; 
+
+%% Only used for KS3:
+% spatial smoothness constant for registration
+ops.sig        = 20;  % default = 20
+
+% blocks for registration. 0 turns it off, 1 does rigid registration. Replaces "datashift" option. 
+ops.nblocks    = 10;  % default = 5
+
+% threshold crossings for pre-clustering (in PCA projection space)
+ops.ThPre = 8; %defauilt=[8]
+
+%%
+step0b_ks_lam(datPath, fs, nCh, ['linear50'], ops, 128)
+%%
+disp('DONE with the first sorting pass WOOOOOOOO0')
+
+end
+
---
+++ Tools/get_sta.m
@@ -0,0 +1,78 @@
+function stas = get_sta( data, targets, use_inds, to_shift, stim_deltas )
+%
+% Usage: stas = get_sta( data, <targets>, <to_shift>, <stim_deltas> )
+%
+% PLAN COMING BACK:
+% -- Make STA function that uses the shifted stim and spikes (and valid data)
+%    It then calls the STA function, as well as shifting function, and reshaping and so on 
+
+%% Now we process the data to make STAs
+binned_SU = [single(data.Robs'), single(data.RobsMU')];
+spk_ch = [data.Robs_probe_ID; data.RobsMU_probe_ID];
+nSU=size(data.Robs,1); 
+
+if nargin < 2 
+    targets = 1:size(binned_SU,2); % default to plotting all SUs and MUs
+end
+
+if nargin <3 || isempty(use_inds)
+    use_inds = data.valid_data;
+    use_inds(end-10:end) = []; %cut last few indices to avoid artifacts
+end
+
+if nargin < 4 || isempty(to_shift)
+    to_shift = 1; 
+end
+
+if nargin < 5
+    stim_deltas = zeros(NT,2); 
+end
+
+stim_shift=permute(data.stim,[4 1 2 3]);
+
+if to_shift~=0
+    stim_shift = shift_stim( stim_shift, data.ETtrace, stim_deltas );
+    shiftstr = 'with shifts';
+else
+    shiftstr = 'no shifts';
+end
+
+%reshaping stimuli to get STAs with matrix multiplication, which is faster
+stim2 = single(reshape(stim_shift,size(stim_shift,1),3*60*60))./127;
+
+
+
+disp('Now plotting!')
+for cc=targets
+	tic
+	figure;
+	for curlag=1:6
+		cur_STA1(curlag,:) = binned_SU(use_inds+curlag,cc)' * stim2(use_inds,:);
+	end
+
+	for curlag=1:6 
+		cur_StA2=reshape(cur_STA1(curlag,:),60,180);
+		%curlim=150;%
+		curlim=max(abs(cur_STA1(:)'));
+		cur_StA2(:,[60 120])=curlim;
+
+		subplot(6,1,curlag)
+		imagesc(cur_StA2); clim([-curlim curlim]); pbaspect([3 1 1])
+
+		colormap(gray); xlabel(['Lag ' num2str(curlag)]);
+		if curlag == 1
+			ylabel('S          L-M          Lum'); 
+            if cc<=nSU;
+                titlestr = sprintf('SU %0d: ch# %d; %s', cc, spk_ch(cc), shiftstr);
+            else
+                titlestr = sprintf('MU %0d: ch# %d; %s', cc-nSU, spk_ch(cc), shiftstr);
+            end
+			%title(['SU ' num2str(cc) ': ch#', num2str(spk_ch(cc)), 'spkID' num2str(spk_ID(cc))]); 
+            title(titlestr)
+		end
+	end
+	%    figtitle(['Probe ' num2str(Robs_probe_ID(cc)) '   Unit ' num2str(cc) ])
+	toc
+	%    pause
+end
+
